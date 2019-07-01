40778d08c6a573867c62a88893ade8ae8756f700
farrelnatan21 <farrel.natan@gmail.com>
2019-07-01 11:43:14 +0700

    penambahan topik

--- pages/user/halaman/home.php
+++ pages/user/halaman/home.php
@@ -7,8 +7,17 @@
   </div>
 
   <div class="form-group">
-    <label for="Nama">Topik</label>
-    <input type="text" class="form-control" id="nama" placeholder="Nama" name="topik" required>
+    <label for="Jurusan">Topik</label>
+    <select name="topik" class="form-control" id="jurusan" name="topik" required>
+    	<option value="Kaki Palsu">Kaki Palsu</option>
+      <option value="Tangan Mekanik Metakarpal">Tangan Mekanik Metakarpal</option>
+      <option value="Exo Skeleton Gen 3">Exo Skeleton Gen 3</option>
+      <option value="Kaki Pelari">Kaki Pelari</option>
+      <option value="Above Knee With Damper">Above Knee With Damper</option>
+      <option value="Morfologi Scanning Kaki">Morfologi Scanning Kaki</option>
+      <option value="PCU">PCU</option>
+      <option value="Control Exo Skeleton">Control Exo Skeleton</option>
+    </select>
   </div>
 
   <div class="form-group">
